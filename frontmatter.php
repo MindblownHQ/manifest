3aa4a98ab269cd9a36da0fe923ea8c2096ab9320
Luc Princen <lucprincen@users.noreply.github.com>
2024-10-15 13:23:55 +0200

    frontmatter

---
+++ frontmatter.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Wooping Shop Health
+ * Plugin URI: https://wooping.io
+ * Description: Shop Health is a free tool for Woocommerce that helps you increase your sales with actionable advice.
+ * Version: 0.9
+ * Author: Wooping
+ * Author URI: https://wooping.io
+ * Text Domain: wooping-shop-health
+ * Requires at least: 6.4
+ * Requires PHP: 7.4
+ * Requires Plugins: woocommerce
+ */
+
+
+define( 'SHOPHEALTH_FILE', __FILE__ );
+define( 'SHOPHEALTH_PATH', dirname( SHOPHEALTH_FILE ) );
+define( 'SHOPHEALTH_VERSION',  '0.9');
+define( 'SHOPHEALTH_DOCUMENTATION_URL', 'https://wooping.io/docs/shop-health/');
